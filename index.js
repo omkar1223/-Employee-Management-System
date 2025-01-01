fe2850d795f3ee3b4617f5207f81a9b1d930519a
omkar1223 <82341191+omkar1223@users.noreply.github.com>
2025-01-01 08:16:41 +0000

    Db data created

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let { role } = require("./models/role.model");
 let { department } = require("./models/department.model");
 let { employee } = require("./models/employee.model");
 let { sequelize } = require("./lib/index");
+let { employeeDepartment } = require("./models/employeeDepartment.model");
+let { employeeRole } = require("./models/employeeRole.model");
 const port = 3000;
 const app = express();
 app.use(express.json());
@@ -33,12 +35,44 @@ app.get("/seed_db", async (req, res) => {
 
   // Associate employees with departments and roles using create method on junction models
 
+  await employeeDepartment.create({
+    employeeId: employees[0].id,
+    departmentId: departments[0].id,
+  });
+  await employeeRole.create({
+    employeeId: employees[0].id,
+    roleId: roles[0].id,
+  });
+
+  await employeeDepartment.create({
+    employeeId: employees[1].id,
+    departmentId: departments[1].id,
+  });
+  await employeeRole.create({
+    employeeId: employees[1].id,
+    roleId: roles[1].id,
+  });
+
+  await employeeDepartment.create({
+    employeeId: employees[2].id,
+    departmentId: departments[0].id,
+  });
+  await employeeRole.create({
+    employeeId: employees[2].id,
+    roleId: roles[2].id,
+  });
+
   return res.json({ message: "Database seeded!" });
 });
 
 app.get("/employees", async (req, res) => {
   const response = await employee.findAll();
   res.status(200).json({ employees: response });
+});
+
+app.get("/departments", async (req, res) => {
+  const response = await department.findAll();
+  res.status(200).json({ departments: response });
 });
 
 app.listen(port, () => {
